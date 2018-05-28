48d28dd2a1ec7b324a1d59d4de7d3a129d08ca42
PennaLia <525772737@qq.com>
2018-05-29 00:08:14 +0800

    修复了login可以打开多个的bug
    现在的login当打开一个的时候不能打开第二个，只有当第一个关闭的时候才能打开

--- src/com/MainApp.java
+++ src/com/MainApp.java
@@ -14,9 +14,17 @@ import java.io.IOException;
 public class MainApp extends Application {
     private Stage primaryStage;
     private AnchorPane rootLayout;
-
-
+    private Boolean login=false,openLogin=false ;//判断是否已经登陆过，如果已经打开登陆窗口，或者已经登陆，就不能再打开登陆窗口
+   //get 和 set
     public AnchorPane getRootLayout(){return  rootLayout;}
+   public boolean getLogin(){return login;}
+    public boolean getOpenLogin(){return openLogin;}
+    public void setLogin(Boolean login) {
+        this.login = login;
+    }
+    public void setOpenLogin(Boolean openLogin) {
+        this.openLogin = openLogin;
+    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -48,20 +56,24 @@ public class MainApp extends Application {
 
   public void login(){
         try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class
-                    .getResource("view/Login.fxml"));
-            AnchorPane page = (AnchorPane) loader.load();
+            if(login==false&&openLogin==false) {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(MainApp.class
+                        .getResource("view/Login.fxml"));
+                AnchorPane page = (AnchorPane) loader.load();
 
-            Stage dialogStage = new Stage();
-            dialogStage.setTitle("Login");
-            dialogStage.initOwner(primaryStage);
-            Scene scene = new Scene(page);
-            dialogStage.setScene(scene);
-            LoginController controller = loader.getController();
-            controller.setDialogStage(dialogStage);
-
-            dialogStage.showAndWait();//只能用户主动关闭
+                Stage dialogStage = new Stage();
+                openLogin=true; //打开了窗口就不能再打开了
+                dialogStage.setTitle("Login");
+                dialogStage.initOwner(primaryStage);
+                Scene scene = new Scene(page);
+                dialogStage.setScene(scene);
+                LoginController controller = loader.getController();
+                controller.setMainApp(this);
+                controller.setDialogStage(dialogStage);
+                controller.setCloseAction();
+                dialogStage.showAndWait();//只能用户主动关闭
+            }
         }catch (Exception e){
             e.getStackTrace();
         }
--- src/com/view/LoginController.java
+++ src/com/view/LoginController.java
@@ -1,10 +1,27 @@
 package com.view;
 
+import com.MainApp;
+import javafx.fxml.FXML;
 import javafx.stage.Stage;
 
 public class LoginController {
     private Stage dialogStage;
+    MainApp mainApp;
+    public void setMainApp(MainApp mainApp){this.mainApp=mainApp;}
     public void setDialogStage(Stage dialogStage){
         this.dialogStage=dialogStage;
     }
+    public Stage getDialogStage() { return dialogStage; }   
+
+    @FXML
+    private void initialize(){
+    }
+    //在main app 那面打开窗口后顺便调用这个方法触发监听事件，当窗口关闭的时候就设置关闭信息，以便下次可以打开
+    public   void setCloseAction(){
+        dialogStage.setOnCloseRequest(windowEvent -> {
+            mainApp.setOpenLogin(false);
+        });
+    }
+
+
 }
