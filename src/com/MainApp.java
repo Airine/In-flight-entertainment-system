3059f84312af9369738a03385cb47119d43e77f3
Aaron <Airine0119@gmail.com>
2018-05-29 20:13:39 +0800

    一些代码简化
    只在MainApp.java

--- src/com/MainApp.java
+++ src/com/MainApp.java
@@ -18,7 +18,7 @@ public class MainApp extends Application {
     private Boolean login=false,openLogin=false ;//判断是否已经登陆过，如果已经打开登陆窗口，或者已经登陆，就不能再打开登陆窗口
    //get 和 set
     public AnchorPane getRootLayout(){return  rootLayout;}
-   public boolean getLogin(){return login;}
+    public boolean getLogin(){return login;}
     public boolean getOpenLogin(){return openLogin;}
     public void setLogin(Boolean login) {
         this.login = login;
@@ -40,7 +40,7 @@ public class MainApp extends Application {
         this.primaryStage.getIcons().add(new Image("resources/shulilogo.png"));
         initRootLayout();
     }
-    public void initRootLayout(){
+    private void initRootLayout(){
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class
@@ -63,7 +63,7 @@ public class MainApp extends Application {
 
   public void login(){
         try {
-            if(login==false&&openLogin==false) {
+            if(!login && !openLogin) {
                 FXMLLoader loader = new FXMLLoader();
                 loader.setLocation(MainApp.class
                         .getResource("view/Login.fxml"));
