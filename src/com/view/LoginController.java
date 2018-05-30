3d0c0da49499516877d77fce8b0f233ecd69bd6c
Aaron <Airine0119@gmail.com>
2018-05-30 11:53:43 +0800

    紧急修复

--- src/com/view/LoginController.java
+++ src/com/view/LoginController.java
@@ -1,18 +1,27 @@
 package com.view;
 
 import com.MainApp;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 
 public class LoginController {
+    public JFXTextField user_name;
+    public JFXButton Login;
+    public JFXButton Sign;
     private Stage dialogStage;
-    MainApp mainApp;
+    private MainApp mainApp;
     public void setMainApp(MainApp mainApp){this.mainApp=mainApp;}
     public void setDialogStage(Stage dialogStage){
         this.dialogStage=dialogStage;
     }
     public Stage getDialogStage() { return dialogStage; }
+
 
     @FXML
     private void initialize(){
@@ -31,4 +40,26 @@ public class LoginController {
     }
 
 
+    public void pressLoginButton(MouseEvent mouseEvent) {
+        Login.setButtonType(JFXButton.ButtonType.FLAT);
+    }
+
+    public void releaseLoginButton(MouseEvent mouseEvent) {
+        Login.setButtonType(JFXButton.ButtonType.RAISED);
+    }
+
+    public void pressSignButton(MouseEvent mouseEvent) {
+        Sign.setButtonType(JFXButton.ButtonType.FLAT);
+    }
+
+    public void releaseSignButton(MouseEvent mouseEvent) {
+        Sign.setButtonType(JFXButton.ButtonType.RAISED);
+    }
+
+    public void getUsernameChanged(InputMethodEvent inputMethodEvent) {
+
+    }
+
+    public void getPwChanged(InputMethodEvent inputMethodEvent) {
+    }
 }
