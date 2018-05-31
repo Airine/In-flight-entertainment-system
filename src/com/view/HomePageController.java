d4cedded102e04445c628f0814a7befcc1fbc64d
PennaLia <525772737@qq.com>
2018-05-31 09:53:56 +0800

    homepage增加了滚动界面

--- src/com/view/HomePageController.java
+++ src/com/view/HomePageController.java
@@ -1,9 +1,39 @@
 package com.view;
 
+import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXMasonryPane;
+import com.jfoenix.controls.JFXScrollPane;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
 public class HomePageController {
+
+    @FXML
+    private JFXScrollPane HomeScrollPane;
+
+    @FXML
+    private JFXMasonryPane HomeMasonryPane;
+
 
     private  RootLayoutController rootLayoutController;
     public void setRootLayoutController(RootLayoutController rootLayoutController){
         this.rootLayoutController=rootLayoutController;
     }
+
+    @FXML
+    private void initialize(){
+
+        for (int i = 0; i < 100; i++) {
+            Label label=new Label();
+            label.setPrefSize(50,50);
+            label.setStyle("-fx-background-color: red");
+            HomeMasonryPane.getChildren().add(label);
+        }
+        HomeScrollPane.setContent(HomeMasonryPane);
+
+    }
+
+
+
 }
