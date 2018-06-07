87c01f23b8cc4035de1494d7fce1bf87370daec5
PennaLia <525772737@qq.com>
2018-06-07 09:21:33 +0800

    Revert "Merge branch 'UI-version-9.5' of https://github.com/Airine/In-flight-entertainment-system into UI-version-9.5"
    This reverts commit 484180eb89963e02876afd70a9223459f440f497, reversing
    changes made to 1f3c3dee5e465a0322b07dc7a453a338e7588c1f.

--- src/com/view/PlayerPageController.java
+++ src/com/view/PlayerPageController.java
@@ -5,9 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
 
 import java.io.IOException;
 
@@ -19,55 +16,41 @@ public class PlayerPageController {
     private StackPane playmovie;//这个用来外接电影的平面
 
     AnchorPane bar;
-    PlayerBarController playerBarController;
+
     //把接入的视频界面赋值给play，然后调用setplaymovie，界面就会替换
     StackPane play;
-    
-    public void setPlay(MediaView mediaView){
-        play = new StackPane();
-        mediaView.setPreserveRatio(true);
-        mediaView.fitHeightProperty().bind(play.heightProperty());
-        play.getChildren().add(mediaView);
-        play.setStyle("-fx-background-color: black;");
+    public void setPlay(StackPane play){
+        this.play=play;
+    }
+    private  void setPlaymovie(){
+        playmovie.getChildren().addAll(play);
     }
 
-    private void setPlaymovie(){
-        playmovie.getChildren().add(play);
-    }
 
-    private RootLayoutController rootLayoutController;
+
+    private  RootLayoutController rootLayoutController;
     public void setRootLayoutController(RootLayoutController rootLayoutController){
         this.rootLayoutController=rootLayoutController;
     }
-    
-    MediaPlayer mediaPlayer;
-    
     @FXML
     private void initialize(){
         try {
-            //这里随便加了个视频并且自动播放
-            mediaPlayer = new MediaPlayer(new Media(
-                    "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"));
-            mediaPlayer.setAutoPlay(true);
-            setPlay(new MediaView(mediaPlayer));
-            setPlaymovie();
             initPlayerBar();
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    private void initPlayerBar(){
+    public void initPlayerBar(){
         try {
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/PlayerBar.fxml"));
-            bar = loader.load();
+            loader.setLocation(MainApp.class
+                    .getResource("view/PlayerBar.fxml"));
+
+            bar=(AnchorPane) loader.load();
             Bar.getChildren().setAll(bar);
-            
-            playerBarController = loader.getController();
+            PlayerBarController playerBarController=new PlayerBarController();
             playerBarController.setPlayPageController(this);
-            playerBarController.controlPlayer(mediaPlayer);
-           
         }catch (IOException e){
             e.printStackTrace();
         }
