082def292ebb29f13cf8da5f5c9838672aabc5f9
PennaLia <525772737@qq.com>
2018-06-09 19:44:06 +0800

    修改了页面
    修改头像有bug，还没有完善
    语言设置马上弄触发事件

--- src/com/view/settingpage/EditController.java
+++ src/com/view/settingpage/EditController.java
@@ -1,11 +1,18 @@
 package com.view.settingpage;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import com.view.RootLayoutController;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class EditController {
+    @FXML
+    private JFXButton choosefile;
 
     @FXML
     private JFXTextField textname;
@@ -14,10 +21,10 @@ public class EditController {
     private AnchorPane edituppane;
     @FXML
     private JFXTextField textsign;
+    String imageURL;
+
     public JFXTextField getTextname(){return  textname;}
     public JFXTextField getTextsign(){return  textsign;}
-
-
     public AnchorPane getEdituppane(){
         return edituppane;
     }
@@ -30,6 +37,17 @@ public class EditController {
         rootLayoutController.getDrawerContentController().changNameAndSign(
                 textname.getText(),textsign.getText()
         );
+   }
+   @FXML
+   public void handleChooseFile(){
+       FileChooser fc=new FileChooser();
+       File seletedFile =fc.showOpenDialog(null);
+       if(seletedFile!=null){
+          imageURL=seletedFile.toURI().toString().substring(5);//加载出文件的路径，绝对路径
+          rootLayoutController.getDrawerContentController().changeUserImage("resources/shiyuan.png");//但是这个加载好像重复了就不行
+       }else {
+           System.out.println("你没有选择文件");
+       }
    }
 
 
--- src/com/view/settingpage/SettingPageController.java
+++ src/com/view/settingpage/SettingPageController.java
@@ -1,10 +1,19 @@
 package com.view.settingpage;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXListView;
 import com.view.RootLayoutController;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.StringConverter;
+
 
 public class SettingPageController {
+    @FXML
+    private JFXComboBox<Label> langComBoix;
+
 
     @FXML
     private AnchorPane settinguppane;
@@ -14,5 +23,30 @@ public class SettingPageController {
     }
     public AnchorPane getSettinguppane(){
         return  settinguppane;
+    }
+    Label chinese=new Label("中文");
+    Label english=new Label("English");
+    Label français=new Label("Français");
+
+    @FXML
+    private void initialize(){
+        initCombox();
+    }
+    private  void initCombox(){
+
+        langComBoix.getItems().addAll(chinese,english,français);
+        langComBoix.setEditable(false);
+        langComBoix.setPromptText("选择语言");
+        langComBoix.setConverter(new StringConverter<Label>() {
+            @Override
+            public String toString(Label object) {
+                return object==null? "" : object.getText();
+            }
+            @Override
+            public Label fromString(String string) {
+                return new Label(string);
+            }
+        });
+
     }
 }
