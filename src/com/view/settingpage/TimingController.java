32aaebaa80f89f750f07c52928bdb1bed0adf58e
PennaLia <525772737@qq.com>
2018-06-10 15:22:40 +0800

    定时关闭的功能已经实现

--- src/com/view/settingpage/TimingController.java
+++ src/com/view/settingpage/TimingController.java
@@ -1,12 +1,29 @@
 package com.view.settingpage;
 
+import com.jfoenix.controls.*;
 import com.view.RootLayoutController;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
+
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TimingController {
     @FXML
     private AnchorPane timinguppane;
+    @FXML
+    private JFXDatePicker choosedata;
+
+
+    @FXML
+    private JFXTimePicker chosetime;
     private RootLayoutController rootLayoutController;
     public void setRootLayoutController(RootLayoutController rootLayoutController){
         this.rootLayoutController=rootLayoutController;
@@ -14,6 +31,40 @@ public class TimingController {
 
     public AnchorPane getTiminguppane() {
         return timinguppane;
+    }
+
+    String closeTime;
+
+    public Timeline timer;
+    public boolean waitForClose=false;
+    int count=0;
+    @FXML
+    public void handleTimingClose(){
+        closeTime=choosedata.getValue().toString().substring(0,5)+choosedata.getValue().toString().substring(6,10)+" "+chosetime.getValue();
+        timer = new Timeline(new KeyFrame(Duration.seconds(10),ev->{
+            if(waitForClose){
+                rootLayoutController.getMainApp().closeWindows();
+            }else {
+                System.out.println("开始"+count);count++;
+                String now = new Date().toLocaleString().toString().substring(0, 15);
+                if (now.equals(closeTime)) {
+                    waitForClose=true;
+                    rootLayoutController.timerOutClose();
+                }
+            }
+        }));
+        timer.setCycleCount(Animation.INDEFINITE);
+        timer.play();
+    }
+
+    class MyTask extends TimerTask {
+
+        //在run方法中的语句就是定时任务执行时运行的语句。
+        public void run() {
+
+        }
+
+
     }
 
     public void loadLanguage(String language) {
