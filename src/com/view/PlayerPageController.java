e6f71f1285e440f13a85e5cbfb07a6c1769198d2
Kemo Huang <11610728@mail.sustc.edu.cn>
2018-06-11 16:16:13 +0800

    加载本地

--- src/com/view/PlayerPageController.java
+++ src/com/view/PlayerPageController.java
@@ -22,6 +22,9 @@ public class PlayerPageController {
     @FXML
     private StackPane playmovie;//这个用来外接电影的平面
 
+    @FXML
+    private StackPane warningPane;        
+    
     AnchorPane bar;
     PlayerBarController playerBarController;
     //把接入的视频界面赋值给play，然后调用setplaymovie，界面就会替换
@@ -29,12 +32,20 @@ public class PlayerPageController {
     public MediaPlayer mediaPlayer;
     private RootLayoutController rootLayoutController;
     
+    public RootLayoutController getRootLayoutController(){
+        return  rootLayoutController;
+    }
+    
     public void setRootLayoutController(RootLayoutController rootLayoutController){
         this.rootLayoutController=rootLayoutController;
     }
 
     public JFXSpinner getSpinner() {
         return spinner;
+    }
+    
+    public StackPane getWarningPane() {
+        return warningPane;
     }
 
     public void setMoviePane(MediaView mediaView){
@@ -50,14 +61,25 @@ public class PlayerPageController {
         playmovie.getChildren().add(MoviePane);
     }
     
-    public RootLayoutController getRootLayoutController(){
-        return  rootLayoutController;
+    public void initPlayerBar(){
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PlayerBar.fxml"));
+            bar = loader.load();
+            Bar.getChildren().setAll(bar);
+            playerBarController = loader.getController();
+            playerBarController.setPlayPageController(this);
+            playerBarController.controlPlayer(mediaPlayer);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
     
     @FXML
     private void initialize(){
         try {
-            //这里随便加了个视频
+            //这里随便加了个小视频
             mediaPlayer = new MediaPlayer(new Media(
                     "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"));
             setMoviePane(new MediaView(mediaPlayer));
@@ -68,21 +90,13 @@ public class PlayerPageController {
         }
     }
     
-
-    public void initPlayerBar(){
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/PlayerBar.fxml"));
-            bar = loader.load();
-            Bar.getChildren().setAll(bar);
-            
-            playerBarController = loader.getController();
-            playerBarController.setPlayPageController(this);
-            playerBarController.controlPlayer(mediaPlayer);
-           
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+    public void setPlayerWithBar(MediaPlayer player){
+        mediaPlayer = player;
+        setMoviePane(new MediaView(player));
+        setPlayMovie();
+        initPlayerBar();
     }
+
+    
 
 }
