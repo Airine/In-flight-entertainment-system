70028081bd67776482dc5f35b07e51840a4d05db
Aaron <Airine0119@gmail.com>
2018-06-12 01:55:02 +0800

    加local

--- src/com/util/DataUpdater.java
+++ src/com/util/DataUpdater.java
@@ -3,11 +3,14 @@ package com.util;
 import com.MainApp;
 import com.model.Movie;
 import com.model.User;
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static com.util.SQLiteJDBC.connectToDB;
 import static com.util.SQLiteJDBC.runSQLstatement;
@@ -66,6 +69,37 @@ public class DataUpdater {
             } catch (Exception e){
                 System.err.println(m+"is already in "+user+"'s collection.");
             }
+        }
+    }
+
+    public static Movie insertLocalMovie(String url){
+        String title = "";
+        String href = "";
+//        String url = "file:/Users/aaron/Documents/GitHub/In-flight-entertainment-system/src/resources/sakai/A-Funny-Thing-Happened-Official-Trailer-1-Michael-Crawford-M.mp4";
+        String parttern = "(\\S*\\/)(\\S*)(\\.mp4)";
+        Pattern r = Pattern.compile(parttern);
+        Matcher m = r.matcher(url);
+        if (m.find()){
+            href = m.group(0).substring(5,m.group(0).length());
+            title =  m.group(2);
+        } else{
+            System.out.println("No match!");
+        }
+        Movie movie = new Movie(1, "本地电影", title, "2018-6-13", "英语", 13, href, "resources/moviePoster/default.png");
+        updateMovie(movie);
+        return movie;
+    }
+
+    public static void main(String args[]){
+        String url = "file:/Users/aaron/Documents/GitHub/In-flight-entertainment-system/src/resources/sakai/A-Funny-Thing-Happened-Official-Trailer-1-Michael-Crawford-M.mp4";
+        String parttern = "(\\S*\\/)(\\S*)(\\.mp4)";
+        Pattern r = Pattern.compile(parttern);
+        Matcher m = r.matcher(url);
+        if (m.find()){
+            System.out.println(m.group(0).substring(5,m.group(0).length()));
+            System.out.println(m.group(2));
+        } else{
+            System.out.println("No match!");
         }
     }
 }
