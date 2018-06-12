d529db18d006f4d5c47204c84e717e2e71e73fd8
Kemo Huang <11610728@mail.sustc.edu.cn>
2018-06-12 23:51:08 +0800

    123

--- src/com/view/viewModel/MovieSortItem.java
+++ src/com/view/viewModel/MovieSortItem.java
@@ -2,31 +2,8 @@ package com.view.viewModel;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.effects.JFXDepthManager;
-import com.jfoenix.svg.SVGGlyph;
-import com.model.Movie;
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
-import javafx.beans.binding.Bindings;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.util.Duration;
-
-import java.util.List;
-import java.util.Random;
-
-import static com.MainApp.mainMovieTypes;
-import static javafx.animation.Interpolator.EASE_BOTH;
-
-/**
- * 
- */
 public class MovieSortItem extends StackPane {
 
     private static final double width = 200;
@@ -56,8 +33,8 @@ public class MovieSortItem extends StackPane {
         VBox.setVgrow(button, Priority.ALWAYS);
         VBox content = new VBox();
 
-        button.setOnMousePressed(event -> {button.setButtonType(JFXButton.ButtonType.FLAT);});
-        button.setOnMouseReleased(event -> {button.setButtonType(JFXButton.ButtonType.RAISED);});
+        button.setOnMousePressed(event -> button.setButtonType(JFXButton.ButtonType.FLAT));
+        button.setOnMouseReleased(event -> button.setButtonType(JFXButton.ButtonType.RAISED));
         this.button = button;
 
         content.getChildren().addAll(button);
