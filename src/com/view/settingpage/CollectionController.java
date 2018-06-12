8345a50ecd05d1f33fafaa6aad1cbc3740c318b8
PennaLia <525772737@qq.com>
2018-06-12 21:08:59 +0800

    colleciton javadoc完成

--- src/com/view/settingpage/CollectionController.java
+++ src/com/view/settingpage/CollectionController.java
@@ -4,13 +4,20 @@ import com.view.RootLayoutController;
 import javafx.fxml.FXML;
 import javafx.scene.layout.FlowPane;
 
-
+/* *  this is class control, just used to contol collection pane
+ * @author PennaLia
+ * @date 2018/6/12 21:07
+ * @version Player Version 1.0
+ */
 public class CollectionController {
+    //this is mycolleciton pane
     @FXML
     private FlowPane mycollection;
+    //this is roolayout controller that connect everything.
+    private RootLayoutController rootLayoutController;
 
     public FlowPane getMycollection(){return mycollection;}
-    private RootLayoutController rootLayoutController;
+
     public void setRootLayoutController(RootLayoutController rootLayoutController){
         this.rootLayoutController=rootLayoutController;
     }
